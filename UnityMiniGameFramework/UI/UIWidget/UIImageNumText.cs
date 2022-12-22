7cdb1f67fe6c10b7ef4581581a6f152403ed988b
ZwjHoi4 <40926628+ZwjHoi4@users.noreply.github.com>
2022-12-22 17:54:37 +0800

    1、字符串格式化工具类。2、生成图片字体文件工具。3、UI库和ImageNum

---
+++ UnityMiniGameFramework/UI/UIWidget/UIImageNumText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace UnityMiniGameFramework
+{
+    public class UIImageNumText : UIImageText
+    {
+        public override string text
+        {
+            get=>base.text;
+            set => base.text = StringUtil.StringNumFormat(value);
+        }
+
+        public void setFont(string fontName)
+        {
+            
+        }
+
+        public void OnPlayEnd()
+        {
+            Destroy(gameObject, 0.1f);
+        }
+        
+        
+        
+    }
+}
---
+++ UnityMiniGameFramework/Utility/StringUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityMiniGameFramework
+{
+    static class StringUtil
+    {
+        // todo：字符串格式化
+        public static string StringNumFormat(string str)
+        {
+            return str;
+        }
+    }
+}
