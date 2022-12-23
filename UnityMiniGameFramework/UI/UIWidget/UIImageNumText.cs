069c41010fccdcb6f819b267cf1a3e98a1cc69d7
ZwjHoi4 <40926628+ZwjHoi4@users.noreply.github.com>
2022-12-23 14:49:36 +0800

    战斗飘字完成
    还未支持暴击时需要改变字体颜色

--- UnityMiniGameFramework/UI/UIWidget/UIImageNumText.cs
+++ UnityMiniGameFramework/UI/UIWidget/UIImageNumText.cs
@@ -1,27 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace UnityMiniGameFramework
 {
     public class UIImageNumText : UIImageText
     {
+        private GameObject m_TargetObject;
+        private RectTransform m_ParentTf;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            m_ParentTf = gameObject.transform.parent.GetComponent<RectTransform>();
+        }
+
+
         public override string text
         {
-            get=>base.text;
+            get => base.text;
             set => base.text = StringUtil.StringNumFormat(value);
         }
 
-        public void setFont(string fontName)
+        public void SetUIPos(GameObject targetObj = null)
         {
-            
+            m_TargetObject = targetObj != null ? targetObj : m_TargetObject;
+            if (m_TargetObject == null)
+                return;
+            if (!(Camera.main is null))
+            {
+                Vector3 screenPoint = Camera.main.WorldToScreenPoint(m_TargetObject.transform.position);
+                Vector3 uiPos = Vector2.zero;
+                RectTransformUtility.ScreenPointToWorldPointInRectangle(m_ParentTf.parent.GetComponent<RectTransform>(),
+                    screenPoint,null, out uiPos);
+                m_ParentTf.position = new Vector3(uiPos.x, uiPos.y + 50, 0);
+            }
         }
+
+        private void FixedUpdate()
+        {
+            SetUIPos();
+        }
+
 
         public void OnPlayEnd()
         {
             Destroy(gameObject, 0.1f);
         }
-        
-        
-        
     }
 }
