3cc9206f5e3b54ca5cda151955d89ba5f26bfbf9
rongjian <309930133@qq.com>
2023-01-05 15:15:02 +0800

    sdk

---
+++ UnityMiniGameFramework/Game/OutGame/StartGameLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityMiniGameFramework
+{
+    public class StartGameLogic
+    {
+        protected UIPanelStartMain _startMainUI;
+
+        public void Init()
+        {
+            _startMainUI = UnityGameApp.Inst.UI.getUIPanel("startMainUI") as UIPanelStartMain;
+        }
+    }
+}
---
+++ UnityMiniGameFramework/SDK/SDKManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace UnityMiniGameFramework
+{
+    public class SDKManager
+    {
+        protected static ISDK _sdk;
+        public static ISDK sdk => _sdk;
+
+        public static void InitSDK(ISDK s)
+        {
+            _sdk = s;
+        }
+    }
+}
